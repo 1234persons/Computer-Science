f9346a87d4b409407d3cd31fe0e314a77d6e49a8
1234persons <Gibreeno@outlook.com>
2023-11-08 19:34:57 -0500

    modified:   Chapter 3/bin/MyFirstApplication.class 	modified:   Chapter 3/src/MyFirstApplication.java 	modified:   Chapter 4/bin/paperWork.class 	modified:   Chapter 4/src/paperWork.java 	modified:   Chapter 5/bin/GuessingGame.class 	modified:   Chapter 5/bin/Hurricane.class 	modified:   Chapter 5/bin/MathTutor.class 	modified:   Chapter 5/bin/PackageCheck.class 	modified:   Chapter 5/bin/QuadraticEquation.class 	modified:   Chapter 5/bin/RandomNum.class 	modified:   Chapter 5/bin/RollingDice.Java 	modified:   Chapter 5/bin/SurfsUp.class 	modified:   Chapter 5/src/RollingDice.Java

--- Chapter 3/src/MyFirstApplication.java
+++ Chapter 3/src/MyFirstApplication.java
@@ -5,7 +5,6 @@
 public class MyFirstApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		System.out.println("Hello World! Gino - \"Have a good day!\"");
 		System.out.print("Hi");
--- Chapter 4/src/paperWork.java
+++ Chapter 4/src/paperWork.java
@@ -2,7 +2,6 @@
 public class paperWork {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 Double x = 7.0;
 Double y = 3.0;
 
