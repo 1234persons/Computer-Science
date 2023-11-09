c182d12b312c4712869f0d1ff2ee05235e4958f8
1234persons <Gibreeno@outlook.com>
2023-11-09 18:45:25 -0500

    new file:   Chapter 6/src/Factorial.java 	modified:   Chapter 6/src/PercentPassing.java

--- Chapter 6/src/PercentPassing.java
+++ Chapter 6/src/PercentPassing.java
@@ -1,3 +1,35 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class PercentPassing {
-    
+    public static void main(String[] args) {
+       
+        Scanner input = new Scanner(System.in);
+        NumberFormat percent = NumberFormat.getPercentInstance();
+       
+        final int FLAG = -1;
+        int userNum = 0;
+        double goodScores = 0;
+        double totalScores = 0;
+        double scoresPercent;
+       
+        while (userNum != FLAG) {
+           
+            System.out.println("Enter a score: (-1 to quit)");
+            userNum = input.nextInt();
+           
+            
+            if (userNum > 70) {
+                goodScores += 1;
+                totalScores += 1;
+            } else {
+                totalScores += 1;
+            }
+        }
+
+        scoresPercent = goodScores/(totalScores - 1);
+
+        System.out.println("The percent of scores above 70% is: " + percent.format(scoresPercent));
+
+    }
 }
