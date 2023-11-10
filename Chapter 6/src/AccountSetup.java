db74feeee8bce392f8d2798cca6eff5f509f4267
1234persons <Gibreeno@outlook.com>
2023-11-09 20:05:31 -0500

    new file:   .vscode/settings.json 	new file:   Chapter 6/src/AccountSetup.java

---
+++ Chapter 6/src/AccountSetup.java
@@ -0,0 +1,25 @@
+import java.util.String;
+import java.util.Scanner;
+
+
+public class AccountSetup {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        String username = " ";
+        String password = " ";
+        
+        while (username.length() < 8) {
+            System.out.println("Enter a username that is at least eight characters: ");
+        username = input.nextLine();
+        }
+
+        while (password.length() < 8) {
+             System.out.println("Enter a password that is at least eight characters: ");
+             password = input.nextLine();
+        }
+
+        System.out.println("Your username is " + username.toLowerCase() + " and your password is " + password.toLowerCase());
+
+    }
+}
