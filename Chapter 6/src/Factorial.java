165952d3027fc5c3dc9b8b3297f34580ce5223a7
1234persons <Gibreeno@outlook.com>
2023-11-09 19:11:16 -0500

    modified:   Chapter 6/src/Factorial.java

--- Chapter 6/src/Factorial.java
+++ Chapter 6/src/Factorial.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Factorial {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         
+        int userNum;
+        int factorial = 1;
+        
+        System.out.println("Enter a number: ");
+        userNum = input.nextInt();
+        input.close();
+        for (int i = 1; i <= userNum; i++ ) {
+            factorial = factorial * i;
+            
+        }
+        
+        System.out.println("The factorial of " + userNum + " is " + factorial);
 
-        
     }
 }
