d8e93d8f2d64bd61055090f9321c0e6c98194b03
1234persons <Gibreeno@outlook.com>
2023-11-10 16:51:06 -0500

    new file:   Chapter 6/src/RandomWalk.java

---
+++ Chapter 6/src/RandomWalk.java
@@ -0,0 +1,39 @@
+
+
+public class RandomWalk {
+    public static void main(String[] args) {
+
+        int startingPoint;
+        double randomStep;
+        int stepCount;
+        int mostSteps = 0; 
+        double averageSteps = 0;
+        double totalSteps = 0;
+        
+        for (int trials = 0; trials < 50; trials++) {
+            stepCount = 0;
+            startingPoint = 0;
+            while (Math.abs(startingPoint) != 7) {
+                
+                randomStep = (10-(-10)) * Math.random() + -10;
+                stepCount += 1;
+                
+                if ((int)randomStep >= 0) {
+                    startingPoint += 1;
+                } else {
+                    startingPoint -= 1;
+                }
+
+                if (mostSteps < stepCount) {
+                    mostSteps = stepCount;
+                }
+                
+
+            }
+            totalSteps += stepCount;
+        }
+        averageSteps = totalSteps/50;
+        System.out.println("The most steps taken was: " + mostSteps);
+        System.out.println("The average amount of steps taken was: " + averageSteps);
+    }
+}
