cf1b1efa19f6248332abf7509ebc5b4835fbfc89
1234persons <Gibreeno@outlook.com>
2023-11-09 19:48:50 -0500

    new file:   Chapter 6/src/OddSum.java

---
+++ Chapter 6/src/OddSum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class OddSum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int userNum;
+        int sum = 0;
+
+        System.out.println("Enter a number: ");
+        userNum = input.nextInt();
+        input.close();
+
+        for (int i = 1; i <= userNum; i++ ) {
+            if (i % 2 == 1) {
+                sum += i;
+            }
+        }
+        System.out.println(userNum);
+        System.out.println("The sum of each odd number is: " + sum);
+    }
+}
