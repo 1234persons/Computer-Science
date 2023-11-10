afab3b2c35eb33ccd5745b2bf8448201387ff4ea
1234persons <Gibreeno@outlook.com>
2023-11-10 12:59:54 -0500

    new file:   Chapter 6/src/ElapsedTimeCalculator.java

---
+++ Chapter 6/src/ElapsedTimeCalculator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.util.String;
+
+public class ElapsedTimeCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        final String MORNING = "am";
+        final String NIGHT = "pm";
+
+        int startingTime;
+        String userDay;
+        int elapsedTime;
+        int endingTime;
+
+        System.out.println("Enter the starting hour: ");
+        startingTime = input.nextInt();
+
+        System.out.println("Enter whether it's am or pm:");
+        userDay = input.next();
+
+        System.out.println("Enter the number of elapsed hours: ");
+        elapsedTime = input.nextInt();
+        input.close();
+
+        endingTime = startingTime + elapsedTime;
+        userDay = userDay.toLowerCase();
+
+        if (endingTime <= 12) {
+            
+            System.out.println(endingTime + ":00 " + userDay);
+
+        } else if (endingTime > 12) {
+          
+            endingTime = endingTime%12;
+           
+            if (userDay.compareToIgnoreCase(MORNING) == 0) { 
+                userDay = NIGHT;
+            } else if (userDay.compareToIgnoreCase(NIGHT) == 0) {
+                userDay = MORNING;
+            }
+
+            System.out.println("The time is: " + endingTime + ":00 " + userDay);
+        }
+
+    }
+}
