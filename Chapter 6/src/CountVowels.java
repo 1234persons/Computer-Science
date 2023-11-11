a225a95b4676b448a785a0d3cbe1dc5484eb00e6
1234persons <Gibreeno@outlook.com>
2023-11-10 20:40:57 -0500

    new file:   Chapter 6/src/CountVowels.java

---
+++ Chapter 6/src/CountVowels.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CountVowels {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String userString;
+        String letter;
+        int vowelCount = 0;
+        int indexMax = 1;
+        System.out.println("Enter a string: ");
+        userString = input.next(); 
+        input.close();
+        for (int index = 0; index < userString.length(); index++) {
+            letter = userString.substring(index, indexMax);
+            if (letter.compareToIgnoreCase("a") == 0 || letter.compareToIgnoreCase("e") == 0 || letter.compareToIgnoreCase("i") == 0 || letter.compareToIgnoreCase("o") == 0 || letter.compareToIgnoreCase("u") == 0) {
+                vowelCount += 1;
+            }
+            indexMax += 1;
+            
+        }
+        System.out.println("The number of vowels in your string are: " + vowelCount);
+    }
+}
