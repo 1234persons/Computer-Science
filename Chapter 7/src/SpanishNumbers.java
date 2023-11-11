72a7d82c1f55130c5ce54698d6acdf305aac7452
1234persons <Gibreeno@outlook.com>
2023-11-11 10:32:16 -0500

    new file:   Chapter 7/src/SpanishNumbers.java

---
+++ Chapter 7/src/SpanishNumbers.java
@@ -0,0 +1,25 @@
+public class SpanishNumbers {
+    public static void SpanishCounting(int i) {
+            switch (i) {
+                case 1: System.out.println("Uno - 1"); break;
+                case 2: System.out.println("Dos - 2"); break;
+                case 3: System.out.println("Tres - 3"); break;
+                case 4: System.out.println("Cuatro - 4"); break;
+                case 5: System.out.println("Cinco - 5"); break;
+                case 6: System.out.println("Seis - 6"); break;
+                case 7: System.out.println("Siete - 7"); break;
+                case 8: System.out.println("Ocho - 8"); break;
+                case 9: System.out.println("Nueve - 9"); break;
+                case 10: System.out.println("Diez - 10"); break;
+            }
+    }
+
+    public static void main(String[] args) {
+        
+        for (int j = 1; j <= 10; j++) {
+            SpanishCounting(j);
+
+
+        }
+    }
+}
