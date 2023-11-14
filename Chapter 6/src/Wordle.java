82e1655ab84950b75e925a172af71d3e3cd8ceb5
1234persons <Gibreeno@outlook.com>
2023-11-14 11:17:29 -0500

    modified:   Chapter 6/src/Wordle.java

--- Chapter 6/src/Wordle.java
+++ Chapter 6/src/Wordle.java
@@ -9,16 +9,47 @@ public class Wordle {
         String userWord;
         String wordList[] = { "BRAIN", "LOSER", "JUNKS", "FUZED", "JOCKS", "COBRA", "QUAKE", "JUICY", "JOKED", "ZESTY",
                 "ADIEU", "STARE", };
-        String secretWord = wordList[rand.nextInt(wordList.length)];
+        //String secretWord = wordList[rand.nextInt(wordList.length)];
+        String secretWord = "BRAIN";
         String RESET = "\u001B[0m";
         String RED = "\u001B[31m";
         String GREEN = "\u001B[32m";
         String YELLOW = "\u001B[33m";
-        int guesses;
+        int guesses = 1;
         boolean correct;
         
-        System.out.println();
+        System.out.println("You will have five attempts to guess a random five letter worc.");
+        System.out.println("Letters in the correct spot will be " + GREEN + "green" + RESET);
+        System.out.println("Letters in the word but not the right spot will be " + YELLOW + "yellow" + RESET);
+        System.out.println("Letters that are not in the word will be " + RED + "red\n" + RESET);
+        
+        while (guesses <= 5) {
+            System.out.println("Enter guess " + guesses + " out of 5");
+            userWord = input.nextLine().toUpperCase();
+
+            for (int i = 0; i < userWord.length(); i++) {
+                Character userChar = userWord.charAt(i);
+                
+                for (int j = 0; j < secretWord.length(); j++) {
+                    Character secretChar = secretWord.charAt(j);
+
+                    if (userChar.equals(secretChar) == true) {
+                        System.out.print(GREEN + userChar + RESET);
+                    }
+                }
+                
+            }
+            
+            if (userWord.equals(secretWord)) {
+                System.out.println("You win");
+                break;
+            } else {
+                guesses += 1;
+            }
+        }
+
+    }
 
         
-    }
 }
+
