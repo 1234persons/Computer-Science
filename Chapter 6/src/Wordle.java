45df8a4b9c8f50841697cf4d8cad8718fd1c22ca
1234persons <Gibreeno@outlook.com>
2023-11-15 14:03:48 -0500

    modified:   Chapter 6/src/Wordle.java

--- Chapter 6/src/Wordle.java
+++ Chapter 6/src/Wordle.java
@@ -1,67 +1,54 @@
 import java.util.Random;
 import java.util.Scanner;
 import java.lang.String;
+import java.util.ArrayList;
 
-public class Wordle {
-    public static void main(String[] args) {
+public class WordleCopy {
+    public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         Random rand = new Random();
         String userWord;
         String wordList[] = { "BRAIN", "LOSER", "JUNKS", "FUZED", "JOCKS", "COBRA", "QUAKE", "JUICY", "JOKED", "ZESTY",
                 "ADIEU", "STARE", };
-        //String secretWord = wordList[rand.nextInt(wordList.length)];
         String secretWord = "BRAIN";
+        String tempWord = secretWord;
         String RESET = "\u001B[0m";
         String RED = "\u001B[31m";
         String GREEN = "\u001B[32m";
         String YELLOW = "\u001B[33m";
-        String tempWord = secretWord;
-        int guesses = 1;
-        boolean correct;
-        
+        String correct = secretWord;
+
         System.out.println("You will have five attempts to guess a random five letter worc.");
         System.out.println("Letters in the correct spot will be " + GREEN + "green" + RESET);
         System.out.println("Letters in the word but not the right spot will be " + YELLOW + "yellow" + RESET);
         System.out.println("Letters that are not in the word will be " + RED + "red\n" + RESET);
-        
-        while (guesses <= 5) {
-            System.out.println("Enter guess " + guesses + " out of 5");
-            userWord = input.nextLine().toUpperCase();
 
-            for (int i = 0; i < userWord.length(); i++) {
-                Character userChar = userWord.charAt(i);
-                
-                for (int j = 0; j < secretWord.length(); j++) {
-                    Character secretChar = secretWord.charAt(j);
-                    Character replacementChar = 0;
-                    if (userChar.equals(secretChar) == true && i != j) {
-                        System.out.print(YELLOW + userChar + RESET);
-                        userWord = userWord.replace(userChar, replacementChar);
-                        break;
-                    } else if (userChar.equals(secretChar) == true) {
-                        System.out.print(GREEN + userChar + RESET);
-                        userWord = userWord.replace(userChar, replacementChar);
-                        break;
-                    }
-                    if (userChar.equals(secretChar) == false) {
-                        System.out.print(RED + userChar + RESET);
-                        userWord = userWord.replace(userChar, replacementChar);
-                        
-                    }
+        for (int round = 0; round < 6; round++) {
+            System.out.print("Enter a guess: ");
+            userWord = input.nextLine().toUpperCase();
+            tempWord = userWord;
+
+            // Create a loop to iterate through each
+            ArrayList<String> usedChars = new ArrayList<String>();
+            for (int i = 0; i < 5; i++) {
+                if (tempWord.equals(correct)) {
+                    System.out.println("You Win!");
+                    break;
+                } else if (userWord.substring(i, i + 1).equals(correct.substring(i, i + 1))) {
+                    // Letter matches
+                    System.out.print(GREEN + userWord.substring(i, i + 1) + RESET);
+                    usedChars.add(userWord.substring(i, i + 1));
+                }else if (usedChars.contains(userWord.substring(i, i + 1))){
+                    System.out.print(RED + userWord.substring(i, i + 1)+ RESET);
+                }else{
+                    System.out.print(YELLOW + userWord.substring(i, i + 1) + RESET);
                 }
-                
+
+                }
+
             }
-            System.out.println();
-            if (userWord.equals(secretWord)) {
-                System.out.println("You win");
-                break;
-            } else {
-                guesses += 1;
-            }
-        }
 
     }
-
-        
 }
 
+    
