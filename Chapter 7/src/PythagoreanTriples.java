a077f80f17e5a5080c48fc07d6c0fd509f0e3faf
1234persons <Gibreeno@outlook.com>
2023-11-17 09:10:34 -0500

    modified:   Chapter 7/bin/PythagoreanTriples.class 	modified:   Chapter 7/src/PythagoreanTriples.java

--- Chapter 7/src/PythagoreanTriples.java
+++ Chapter 7/src/PythagoreanTriples.java
@@ -1,31 +1,8 @@
-import java.util.Math;
+import java.lang.Math;
 
 public class PythagoreanTriples {
     public static void main(String[] args) {
-        double c;
-        for (double a = 1; a <= 100; a++) {
-            perfectSquareA = perfectSquare(a, perfectSquareA);
-            if (perfectSquareA == true) {
-                for (int b = 1; b <= 100; b++) {
-                    perfectSquareB = perfectSquare(b, perfectSquareB);
-                    if (perfectSquareB == true) {
-                        a = Math.pow(a);
-                        b = Math.pow(b);
-                        c = a + b;
-                        double tempC = c;
-                        
-                        c = perfectSquare(c);
-                        if (c == tempC) {
-                            System.out.println(a + ", " + b + ", " + c);
-                        } else {
-                            break;
-                        }
-                    }
-                }
-           }
-        }
-        System.out.println("Cheese");
-
+        
     }
     public static double perfectSquare(double num) {
         double square;
