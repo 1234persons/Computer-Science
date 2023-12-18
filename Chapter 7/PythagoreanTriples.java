306ba9258c3bada096cff5061adea5f439f60fb5
1234persons <150205031+1234persons@users.noreply.github.com>
2023-12-18 12:51:44 -0500

    Update PythagoreanTriples.java
    I hate this program.

--- Chapter 7/PythagoreanTriples.java
+++ Chapter 7/PythagoreanTriples.java
@@ -1,32 +1,62 @@
-import java.util.math;
-
-
 public class PythagoreanTriples {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        
-    }
-=======
-       
-    }
-    public static boolean perfectSquare(double num) {
-                double originalNumber = num;
-                double square;
-                originalNumber = num;
-                
-                square = Math.sqrt(num);
-                square = (double)square;
-                square = Math.pow((int)square, 2);
-                
-                if (square == originalNumber) {
-                    return(true);
-                } else {
-                    return(false);
-                }
-                
-                
-                
-        
+
+        for (int a = 1; a <= 100; a++) {
+
+            //if (perfectSquare(a)) {
+
+                for (int b = 1; b <= 100; b++) {
+
+                    //if (perfectSquare(b)) {
+
+                        for (int c = 1; c <= 100; c++) {
+
+                            if (Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2) && a < b && coPrime(a, b)) {
+                                System.out.println(a + ", " + b + ", " + c);
+                            }
+
+                        }
+
+                    }
+                    
+                //}
+
             }
->>>>>>> 4c3e0444b24eae7c97a42d80ea9204bc0c041c75
+
+        //}
+
+    }
+
+    // public static boolean perfectSquare(double num) {
+    //     double originalNumber = num;
+    //     double square;
+    //     originalNumber = num;
+
+    //     square = Math.sqrt(num);
+    //     square = (double) square;
+    //     square = Math.pow((int) square, 2);
+
+    //     if (square == originalNumber) {
+    //         return (true);
+    //     } else {
+    //         return (false);
+    //     }
+
+    // }
+
+    public static boolean coPrime(int m, int n) {
+
+        /* Since m <= n */
+        int index = 2;
+        boolean is_coprime = true;
+        while (index <= m) {
+            if (m % index == 0 && n % index == 0) {
+                is_coprime = false;
+            }
+            index = index + 1;
+        }
+        return is_coprime;
+    }
+
 }
+
