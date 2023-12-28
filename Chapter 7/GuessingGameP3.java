08257413b3adb2143936428a2cde673797d5fb97
1234persons <150205031+1234persons@users.noreply.github.com>
2023-12-28 11:55:47 -0500

    Create GuessingGameP3.java

---
+++ Chapter 7/GuessingGameP3.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGameP3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+		Random rand =  new Random();
+		 
+        int userNum;
+        int randomNum;
+
+        randomNum = rand.nextInt(1, 20);
+        System.out.println(randomNum);
+        do {
+    	 System.out.println("Enter a number between 1 and 20: ");
+         userNum = input.nextInt();
+         hint(userNum, randomNum);
+         
+        } while (userNum != randomNum);
+        System.out.println("You Win!!!!!!");
+     input.close();
+    }
+
+    public static void hint(int userNum, int randomNum) {
+        if (userNum < randomNum) {
+            System.out.println("Higher");
+        } else if (userNum > randomNum) {
+            System.out.println("Lower");
+        }
+    }
+
+}
