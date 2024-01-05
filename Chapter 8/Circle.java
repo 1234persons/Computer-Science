9011818ec618e62c8205027dc12341e363b949bd
1234persons <150205031+1234persons@users.noreply.github.com>
2024-01-05 08:36:23 -0500

    Update Circle.java
    I don't know how to read this I just copied it from the textbook for the love of Christ help me.

--- Chapter 8/Circle.java
+++ Chapter 8/Circle.java
@@ -1,3 +1,42 @@
+import java.text.NumberFormat;
+
 public class Circle {
+    private static NumberFormat number = NumberFormat.getNumberInstance();
+    private static final double PI = 3.14;
+    private double radius;
+
+    public static void main(String[] args) {
+        Circle spot = new Circle();
+
+        spot.setRadius(5);
+
+        System.out.println("The radius is " + spot.getRadius());
+        System.out.println("The area is " + spot.area());
+        System.out.println("The circumference is " + number.format(spot.circumference()));
+    }
     
+    public Circle() {
+        radius = 1;
+    }
+
+    public void setRadius(double newRadius) {
+        radius = newRadius;
+    }
+
+    public double circumference() {
+        double circumference = 2 * PI * radius;
+
+        return(circumference);
+    }
+
+    public double area() {
+        double circleArea = PI * radius * radius;
+
+        return(circleArea);
+    }
+
+    public double getRadius() {
+        return(radius);
+    }
 }
+
