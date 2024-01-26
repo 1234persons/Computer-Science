a863872c81c83e0f23a78788a76204c34449d975
1234persons <150205031+1234persons@users.noreply.github.com>
2024-01-26 16:10:31 -0500

    Update CircleMain.java

--- Chapter 8/Circle/CircleMain.java
+++ Chapter 8/Circle/CircleMain.java
@@ -1,11 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
-        Circle spot = new Circle();
+package Circle;
 
-        spot.setRadius(5);
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class CircleMain {
+    public static void main(String[] args) {
+
+        Circle spot = new Circle();
+        Circle spotP2 = new Circle(5);
+
+        NumberFormat number = NumberFormat.getNumberInstance();
+
+        spot.setRadius(10);
 
         System.out.println("The radius is " + spot.getRadius());
         System.out.println("The area is " + spot.area());
-        System.out.println("The circumference is " + number.format(spot.circumference()));
+        System.out.println("The circumference is " + number.format(spot.circumference()) + "\n");
+
+        System.out.println("The radius of the second circle is: " + spotP2.getRadius());
+        System.out.println("The area is: " + spotP2.area());
+        System.out.println("The circumference is: " + number.format(spotP2.circumference()) + "\n");
+
+        Circle.displayAreaFormula();
     }
 }
