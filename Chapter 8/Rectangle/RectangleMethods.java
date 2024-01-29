e21e6124b8a96f398f8fd784c91950785d034ba0
1234persons <150205031+1234persons@users.noreply.github.com>
2024-01-29 10:13:02 -0500

    Update RectangleMethods.java

--- Chapter 8/Rectangle/RectangleMethods.java
+++ Chapter 8/Rectangle/RectangleMethods.java
@@ -1,1 +1,30 @@
+package Rectangle;
+public class Rectangle {
+    
+    private double width;
+    private double length;
+
+    public Rectangle() {
+        width = 1;
+        length = 1;
+    }
+
+    public Rectangle(double newWidth, double newLength) {
+        width = newWidth;
+        length = newLength;
+    }
+
+    public double area() {
+        return(length * width);
+    }
+
+    public double perimeter() {
+        return(2 * length + 2 * width);
+    }
+
+    public static void displayAreaFormula() {
+        System.out.println("The formula for the area of a rectangle is Width * Length");
+    }
+
+}
 
