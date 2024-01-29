4af9bb680eb52e8d180b84f277ffb9ef2ef33d39
1234persons <150205031+1234persons@users.noreply.github.com>
2024-01-29 10:11:39 -0500

    Update RectangleMain.java

--- Chapter 8/Rectangle/RectangleMain.java
+++ Chapter 8/Rectangle/RectangleMain.java
@@ -1,1 +1,19 @@
+package Rectangle;
+
+public class RectangleMain {
+    public static void main(String[] args) {
+
+        Rectangle rect = new Rectangle();
+        Rectangle rect2 = new Rectangle(5, 2);
+
+        System.out.println("\nThe area of rectangle one is: " + rect.area());
+        System.out.println("The perimeter of rectangle one is: " + rect.perimeter() + "\n");
+
+        System.out.println("The area of rectangle two is: " + rect2.area());
+        System.out.println("The perimeter of rectangle two is: " + rect2.perimeter() + "\n");
+
+        Rectangle.displayAreaFormula();
+
+    }
+}
 
