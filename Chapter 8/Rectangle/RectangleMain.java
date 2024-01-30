893cec6450f497d5921fd7f6f06a4c56ae53f3ee
1234persons <150205031+1234persons@users.noreply.github.com>
2024-01-30 11:21:31 -0500

    Update RectangleMain.java

--- Chapter 8/Rectangle/RectangleMain.java
+++ Chapter 8/Rectangle/RectangleMain.java
@@ -6,6 +6,15 @@ public class RectangleMain {
         Rectangle rect = new Rectangle();
         Rectangle rect2 = new Rectangle(5, 2);
 
+        if (rect.equals(rect2)) {
+            System.out.println("Objects are equal");
+        } else {
+            System.out.println("Objects are not equal");
+        }
+
+        System.out.println(rect);
+        System.out.println(rect2);
+
         System.out.println("\nThe area of rectangle one is: " + rect.area());
         System.out.println("The perimeter of rectangle one is: " + rect.perimeter() + "\n");
 
@@ -16,4 +25,3 @@ public class RectangleMain {
 
     }
 }
-
