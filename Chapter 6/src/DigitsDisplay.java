e1f0141c7338d9b0190b56eb9c0d43b3b5f8ebd8
1234persons <gibreeno@outlook.com>
2024-01-31 14:09:25 -0500

    modified:   Chapter 6/bin/DigitsDisplay.class     modified:   Chapter 6/src/DigitsDisplay.java 	new file:   Coding Challenge/CodingChallenge1.java

--- Chapter 6/src/DigitsDisplay.java
+++ Chapter 6/src/DigitsDisplay.java
@@ -1,18 +1,17 @@
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class DigitsDisplay {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int num;
-		ArrayList digits[] = new int[];
+
 
 		System.out.print("Ener a number: \n");
 		num = input.nextInt();
 		input.close();
 
 		while (num >= 1) {
-			digits.add(num % 10);
 			System.out.println(num % 10);
 			num /= 10;
 
---
+++ Coding Challenge/CodingChallenge1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CodingChallenge1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int userNum;
+
+        do {
+            System.out.println("Enter a positive number: ");
+            userNum = input.nextInt();
+        } while (userNum <= 0);
+
+        for (int i = 1; i <= userNum; i++) {
+            if (i % 4 == 0 && i % 6 == 0) {
+                continue;
+            } else if (i % 4 == 0 || i % 6 == 0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
