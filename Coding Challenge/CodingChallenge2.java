7dfe1920f4066c4ef1fe937b7076f9334ba4f638
1234persons <gibreeno@outlook.com>
2024-02-14 13:32:27 -0500

    modified:   Coding Challenge/CodingChallenge2.java

--- Coding Challenge/CodingChallenge2.java
+++ Coding Challenge/CodingChallenge2.java
@@ -5,12 +5,9 @@ public class CodingChallenge2 {
         Scanner input = new Scanner(System.in);
 
         int userNum;
-        int nextUserNum;
-        int change;
         int changeSum = 0;
 
         do {
-
             do {
                 System.out.println("Enter your current height above sea level: (-1 to quit)");
                 userNum = input.nextInt();
@@ -19,19 +16,12 @@ public class CodingChallenge2 {
             if (userNum == -1) {
                 break;
             }
-
-            do {
+ 
             System.out.println("Enter your next height: ");
-            nextUserNum = input.nextInt();
-            } while (nextUserNum < 1);
+            changeSum += Math.abs(userNum - input.nextInt());
 
-            change = Math.abs(userNum - nextUserNum);
-            changeSum += change;
-            
-            System.out.println("The sum of the changes in height is: " + changeSum);
+            System.out.println("The sum of the changes in height is: " + changeSum + "\n");
         } while (true);
-
-        System.out.println("Have a good day!");
         input.close();
     }
 }
