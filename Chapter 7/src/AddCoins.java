4df3e1c5dad99d45de086e4d3d78f860e8204209
1234persons <gibreeno@outlook.com>
2024-03-26 11:07:36 -0400

    modified:   Chapter 7/bin/AddCoins.class 	modified:   Chapter 7/src/AddCoins.java

--- Chapter 7/src/AddCoins.java
+++ Chapter 7/src/AddCoins.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 import java.text.NumberFormat;
 
 public class AddCoins {
-    
+
     public static void main(String[] args) {
         
-    
-    Scanner input = new Scanner(System.in);
+
+        Scanner input = new Scanner(System.in);
 
         double quarters;
         double dimes;
         double nickels;
         double pennies;
+        String total;
 
         System.out.println("Enter the amount of quarters: ");
         quarters = input.nextDouble();
@@ -21,26 +22,24 @@ public class AddCoins {
 
         System.out.println("Enter the amount of nickels: ");
         nickels = input.nextDouble();
-        
+
         System.out.println("Enter the amount of pennies: ");
         pennies = input.nextDouble();
 
-        getDollarAmount(quarters, dimes, nickels, pennies);
+        total = getDollarAmount(quarters, dimes, nickels, pennies);
+
+        System.out.println("The total is: " + total);
         input.close();
     }
-    public static void getDollarAmount(double quarters, double dimes, double nickels, double pennies) {
+
+    public static String getDollarAmount(double quarters, double dimes, double nickels, double pennies) {
         NumberFormat money = NumberFormat.getCurrencyInstance();
-        quarters = (quarters/100) * 25;
-        dimes = (dimes/100) * 10;
-        nickels = (nickels/100) * 5;
-        pennies = pennies/100;
-        double dollarAmount;
-        
-        dollarAmount = quarters + dimes + nickels + pennies;
-
-        System.out.println("The total is: " + money.format(dollarAmount));
-
+        quarters = (quarters / 100) * 25;
+        dimes = (dimes / 100) * 10;
+        nickels = (nickels / 100) * 5;
+        pennies = pennies / 100;
+        String dollarAmount = money.format(quarters + dimes + nickels + pennies);
+        return(dollarAmount);
 
     }
 }
-
